accdc6759a1db7ba0634562819a9a04d1024f266
Crowdbotics <team@crowdbotics.com>
2020-11-13 17:56:56 +0000

    Update components

--- src/features/BlankScreen114503/screens/blank.js
+++ src/features/BlankScreen114503/screens/blank.js
@@ -23,13 +23,62 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = {
+    TextInput_6: "",
+    TextInput_8: "",
+    DateTimePicker_10: new Date(""),
+    TextInput_12: "",
+    Switch_14: true,
+    CheckBox_110: true,
+    CheckBox_108: true
+  }
 
   render = () => (
     <View>
       <Button title="Press me!" onPress={() => alert("Pressed!")} />
-      <Button title="Press me!" onPress={() => alert("Pressed!")} />
-      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Text style={styles.Text_4}>Sample text content</Text>
+      <TextInput
+        placeholder="Sample text input placeholder"
+        value={this.state.TextInput_6}
+        onChangeText={nextValue => this.setState({ TextInput_6: nextValue })}
+      />
+      <TextInput
+        placeholder="Number Input Placeholder"
+        keyboardType="numeric"
+        value={this.state.TextInput_8}
+        onChangeText={nextValue => this.setState({ TextInput_8: nextValue })}
+      />
+      <DateTimePicker
+        showIcon={false}
+        date={this.state.DateTimePicker_10}
+        onDateChange={selectedDate =>
+          this.setState({ DateTimePicker_10: selectedDate })
+        }
+      />
+      <TextInput
+        placeholder="Sample text input placeholder"
+        multiline={true}
+        value={this.state.TextInput_12}
+        onChangeText={nextValue => this.setState({ TextInput_12: nextValue })}
+      />
+      <Switch
+        trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
+        style={styles.Switch_14}
+        value={this.state.Switch_14}
+        onValueChange={nextChecked => this.setState({ Switch_14: nextChecked })}
+      />
+      <CheckBox
+        title="Checkbox"
+        checked={this.state.CheckBox_110}
+        onPress={nextChecked => this.setState({ CheckBox_110: nextChecked })}
+      />
+      <CheckBox
+        title="Radio button"
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
+        checked={this.state.CheckBox_108}
+        onPress={nextChecked => this.setState({ CheckBox_108: nextChecked })}
+      />
     </View>
   )
 }
@@ -39,8 +88,19 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16
   },
+
   View_1: {},
   Button_2: {},
   Button_3: {},
-  Button_4: {}
+  Button_4: {},
+  View_1: {},
+  Button_2: {},
+  Text_4: { textAlign: "left" },
+  TextInput_6: {},
+  TextInput_8: {},
+  DateTimePicker_10: {},
+  TextInput_12: {},
+  Switch_14: { alignSelf: "flex-start" },
+  CheckBox_110: {},
+  CheckBox_108: {}
 })
