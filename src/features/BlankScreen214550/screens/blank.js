0df66db55017f7f62e25a7daeb404c0f45023949
Crowdbotics <team@crowdbotics.com>
2020-11-17 22:41:52 +0000

    Update components

--- src/features/BlankScreen214550/screens/blank.js
+++ src/features/BlankScreen214550/screens/blank.js
@@ -27,11 +27,11 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View>
-      <Image
-        source={{
-          uri:
-            "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/94228506_2626628984246508_3570203556348690432_o_6eGLDRS.jpg"
-        }}
+      <Image />
+      <Button
+        title="Press me!"
+        color="#ff338f"
+        onPress={() => alert("Pressed!")}
       />
     </View>
   )
@@ -126,5 +126,8 @@ const styles = StyleSheet.create({
   View_1: {},
   Image_3: {},
   View_1: {},
-  Image_3: {}
+  Image_3: {},
+  View_1: {},
+  Image_3: {},
+  Button_5: {}
 })
