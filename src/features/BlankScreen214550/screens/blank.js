748a2caf8f6a8cc1b1f0fda6509b387c421584f8
Crowdbotics <team@crowdbotics.com>
2020-11-17 22:41:37 +0000

    Update components

--- src/features/BlankScreen214550/screens/blank.js
+++ src/features/BlankScreen214550/screens/blank.js
@@ -27,7 +27,12 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View>
-      <Image />
+      <Image
+        source={{
+          uri:
+            "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/94228506_2626628984246508_3570203556348690432_o_6eGLDRS.jpg"
+        }}
+      />
     </View>
   )
 }
@@ -118,6 +123,8 @@ const styles = StyleSheet.create({
   Image_3: {},
   Button_5: {},
   View_1: {},
+  View_1: {},
+  Image_3: {},
   View_1: {},
   Image_3: {}
 })
