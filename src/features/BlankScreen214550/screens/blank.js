c9595988e4b9d7e4dbc716f9d5fb055e27b538fe
Crowdbotics <team@crowdbotics.com>
2020-11-18 10:31:26 +0000

    Update components

--- src/features/BlankScreen214550/screens/blank.js
+++ src/features/BlankScreen214550/screens/blank.js
@@ -25,7 +25,11 @@ export default class Blank extends React.Component {
 
   state = {}
 
-  render = () => <View />
+  render = () => (
+    <View>
+      <Image />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -131,5 +135,7 @@ const styles = StyleSheet.create({
   View_1: {},
   View_1: {},
   Image_3: {},
-  View_1: {}
+  View_1: {},
+  View_1: {},
+  Image_3: {}
 })
