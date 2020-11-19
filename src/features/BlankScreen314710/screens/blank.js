3f0a101f48c31925537eeeaa0562bbb176c65448
Crowdbotics <team@crowdbotics.com>
2020-11-19 20:06:51 +0000

    Update components

--- src/features/BlankScreen314710/screens/blank.js
+++ src/features/BlankScreen314710/screens/blank.js
@@ -28,11 +28,12 @@ export default class Blank extends React.Component {
   render = () => (
     <View>
       <Switch
-        trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
+        trackColor={{}}
         style={styles.Switch_3}
         value={this.state.Switch_3}
         onValueChange={nextChecked => this.setState({ Switch_3: nextChecked })}
       />
+      <Button title="Press me!123" onPress={() => alert("Pressed!")} />
     </View>
   )
 }
@@ -42,6 +43,10 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16
   },
+
   View_1: {},
-  Switch_3: { alignSelf: "flex-start" }
+  Switch_3: { alignSelf: "flex-start" },
+  View_1: {},
+  Switch_3: { alignSelf: "flex-start" },
+  Button_5: {}
 })
