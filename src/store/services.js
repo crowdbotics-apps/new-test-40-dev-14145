092bcd8421da977889969a5c3321f2254426f9f6
Crowdbotics <team@crowdbotics.com>
2020-11-20 22:31:28 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,4 +1,9 @@
 import axios from "axios"
+const plugin = axios.create({
+  baseURL:
+    "https://crowdbotics-slack-dev.herokuapp.com/dashboard/app/7307/storyboard/8491/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 const nEWTESTAPI = axios.create({
   baseURL: "https://new-test-40-dev-14145-prod.herokuapp.com/",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
