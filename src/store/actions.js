d7f88522c836bac79bce235cdae021c4b45876e0
Crowdbotics <team@crowdbotics.com>
2020-11-20 22:38:36 +0000

    Update store

--- src/store/actions.js
+++ src/store/actions.js
@@ -356,16 +356,19 @@ export const rest_auth_user_partial_updateFailed = (error, starter) => ({
   error,
   starter
 })
+
 export const newpluginxxxx_get__read = (new_param, new_param_2) => ({
   type: types.NEWPLUGINXXXX_GET__READ,
   new_param,
   new_param_2
 })
+
 export const newpluginxxxx_get__readSucceeded = (response, starter) => ({
   type: types.NEWPLUGINXXXX_GET__READ_SUCCEEDED,
   response,
   starter
 })
+
 export const newpluginxxxx_get__readFailed = (error, starter) => ({
   type: types.NEWPLUGINXXXX_GET__READ_FAILED,
   error,
